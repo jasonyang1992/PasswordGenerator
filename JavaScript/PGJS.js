f0e96b7b369bb398d4f31f438fe8a1ab9931cb9a
Jason <jasonyang1992@gmail.com>
2019-06-13 12:37:36 -0400

    Major Update
    Complete Overhaul of the code

--- JavaScript/PGJS.js
+++ JavaScript/PGJS.js
@@ -1,169 +1,33 @@
 function PassGenFunction() {
 
-  var x = document.getElementById("NumLet").value;
-  var y = document.getElementById("NumNum").value;
-  var z = document.getElementById("NumSym").value;
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var xyz = x + y + z;
+  var LetCounter = document.getElementById("NumLet").value; //Letter
+  var NumCounter = document.getElementById("NumNum").value; // Numbers
+  var SymCounter = document.getElementById("NumSym").value; // Symbols
 
-  var threeword = [
-    "San",
-    "Cow",
-    "Tan",
-    "Ran",
-    "Gam",
-    "Sad",
-    "Top",
-    "Kev",
-    "Tra",
-    "Fan",
+  var possibleCont = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; // Letter
+  var possibleVowel = "AEIOUaeiou"  //Vowels
+  var possibleSym = "!@#$%^&*()" // Symbols
+  var FinalResult = ""; //String result
 
-    "Jan",
-    "Wat",
-    "Opp",
-    "Lot",
-    "Van",
-    "Sat",
-    "Nom",
-    "Pot",
-    "Vip",
-    "Mix",
-
-    "Wet",
-    "Rat",
-    "Pan",
-    "Qan",
-    "Iot",
-    "Vet",
-    "Xan",
-    "Sat",
-    "Kil",
-    "Get",
-
-    "Fat",
-    "Jum",
-    "Hap",
-    "Sho",
-    "Ret",
-    "Rea",
-    "Dog",
-    "Cat",
-    "Xan",
-    "Jot",
-
-    "Tam",
-    "Rex",
-    "Row",
-    "Max",
-    "Joy",
-    "Mov",
-    "Toy",
-    "Sed",
-    "Boy",
-    "Vet",
-
-    "Zed",
-    "Wat",
-    "kin",
-    "Kon",
-    "Lee",
-    "Pap",
-    "Hen",
-    "Fip",
-    "Bet",
-    "Jet",
-
-    "Ret",
-    "Beb",
-    "Jop",
-    "Job",
-    "Net",
-    "Ner",
-    "Kat",
-    "Mat",
-    "Jay",
-    "Atm"
-
-  ]
-
-  var Symbols1 = [
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")"
-  ]
-
-  var n = 70; // This is the number of words in the array
-
-  if (x == 1){
-    var Letresult = "";
-    for (var i = 0; i < 1; i++)
-    Letresult += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  }
-  if (x == 2){
-    var Letresult = "";
-    for (var i = 0; i < 2; i++)
-    Letresult += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  }
-  if (x == 3){
-  var Letresult = threeword[Math.floor(Math.random() * n)]
-
-  }
-  if (x == 4){
-    for (var i = 0; i < 1; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-    var Letresult = text + threeword[Math.floor(Math.random() * n)];
-
-  }
-  if (x == 5){
-    for (var i = 0; i < 2; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-    var Letresult = text + threeword[Math.floor(Math.random() * n)];
-
-  }
-  if (x == 6){
-    var Letresult = threeword[Math.floor(Math.random() * n)] + threeword[Math.floor(Math.random() * n)];
-
+// Generate random Letters
+  for (var i = 0; i < LetCounter; i++) {
+      if ((i - 1) % 3 == 0) {
+        FinalResult += possibleVowel.charAt(Math.floor(Math.random() * possibleVowel.length));
+      }
+      else{
+        FinalResult += possibleCont.charAt(Math.floor(Math.random() * possibleCont.length));
+      }
   }
 
-
-
-  if (y == 1){
-  var Numresult = Math.floor(Math.random() * 10);
-
-  }
-  if (y == 2){
-  var Numresult = Math.floor(Math.random() * 90) + 10;
-
-  }
-  if (y == 3){
-  var Numresult = Math.floor(Math.random() * 900) + 99;
-
-  }
-  if (y == 4){
-  var Numresult = Math.floor(Math.random() * 9000) + 999;
-
+// Generate random Symbols
+  for (var i = 0; i < SymCounter; i++ ) {
+    FinalResult += possibleSym.charAt(Math.floor(Math.random() * possibleSym.length));
   }
 
-
-
-  if (z == 1){
-    var Symresult = Symbols1[Math.floor(Math.random() * 10)]
-
-  }
-  if (z == 2){
-    var Symresult = Symbols1[Math.floor(Math.random() * 10)] + Symbols1[Math.floor(Math.random() * 10)];
-
+//Generate random Numbers
+  for (var i = 0; i < NumCounter; i++ ) {
+    FinalResult += Math.floor(Math.random() * 10);
   }
 
-  document.getElementById("Password").innerHTML = Letresult + Numresult + Symresult;
+  document.getElementById("Password").innerHTML = FinalResult; // Final result
 }
